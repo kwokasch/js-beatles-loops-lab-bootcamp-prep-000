be5369881dea0e6f3d7a12b9a5c070a8c5cb5117
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-23 00:13:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function johnLennonFacts(facts){
   var result = [];
   
   for(let i=0; i<facts.length; i++){
-    result.push(test[i]+'!!!');
+    result.push(facts[i]+'!!!');
   }
   return result;
 }
