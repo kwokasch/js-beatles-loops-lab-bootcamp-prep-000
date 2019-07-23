179eff933eab3870c137f4052810a0ef531b5104
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-23 00:12:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function theBeatlesPlay(musicians, instruments){
   }
   return newArr;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts){
+  var result = [];
+  
+  for(let i=0; i<facts.length; i++){
+    result.push(test[i]+'!!!');
+  }
+  return result;
+}
